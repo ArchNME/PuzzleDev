c94ff6e8d8c22abc93847086afe9721faf2f18b7
Viability <viability@live.com>
2012-11-02 02:22:11 -0600

    Commit
    hahah

--- PuzzleDev/src/com/xviable/PuzzleDev/MovementListener.java
+++ PuzzleDev/src/com/xviable/PuzzleDev/MovementListener.java
@@ -2,10 +2,14 @@ package com.xviable.PuzzleDev;
 
 import java.util.ArrayList;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.Vector;
 
 public class MovementListener implements Listener {
 
@@ -22,16 +26,24 @@ public class MovementListener implements Listener {
 	}
 
 	@EventHandler
-	public void onPlayerInteract(EntityInteractEvent event) {
-		Entity entity = event.getEntity();
-
-		System.out.println("Player Movement Detected");
-
-		for (int i = 0; i < jumpPads.size(); i++) {
-			System.out.println("JumpPad Checker Running(" + i + ")");
-			if (entity.getLocation() == jumpPads.get(i).getLocation()) {
-				jumpPads.get(i).launch(entity);
-			}
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		Player p = event.getPlayer();
+		System.out.println("Player Movement Detected : " + event.getAction());
+		if (event.getAction() == event.getAction().PHYSICAL) {
+			System.out.println("SUCCESS");
+			Vector v = p.getVelocity();
+			v.setY(0.5);
+			v.setX(v.getX()*0.5);
+			v.setZ(v.getZ()*0.5);
+			p.setVelocity(v);
 		}
+		
+
+//		for (int i = 0; i < jumpPads.size(); i++) {
+//			System.out.println("JumpPad Checker Running(" + i + ")");
+//			if (entity.getLocation() == jumpPads.get(i).getLocation()) {
+//				jumpPads.get(i).launch(entity);
+//			}
+//		}
 	}
 }
