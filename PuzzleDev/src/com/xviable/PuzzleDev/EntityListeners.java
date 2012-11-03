ef266fa1fc9abf433aaa2212a54cf3e63b1fa91b
Viability <viability@live.com>
2012-11-03 11:38:17 -0600

    Added Squid Carrying

--- PuzzleDev/src/com/xviable/PuzzleDev/EntityListeners.java
+++ PuzzleDev/src/com/xviable/PuzzleDev/EntityListeners.java
@@ -2,15 +2,23 @@ package com.xviable.PuzzleDev;
 
 import java.util.ArrayList;
 
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.craftbukkit.entity.CraftChicken;
+import org.bukkit.craftbukkit.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.craftbukkit.entity.CraftSquid;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.material.Diode;
 import org.bukkit.util.Vector;
@@ -59,15 +67,16 @@ public class EntityListeners implements Listener {
 					for (int x = -1; x <= 1; x++) {
 						for (int y = -1; y <= 1; y++) {
 							if ((x * x + y * y + z * z == 1) && b.getRelative(x, y, z).getTypeId() == 94 || (x * x + y * y + z * z == 1) && b.getRelative(x, y, z).getTypeId() == 93) {
-								System.out.println("Diode Found");
+								Location loc = b.getRelative(0,1,0).getLocation();
+								System.out.println(b.getRelative(0,1,0).getLocation());
+								Vector reset = new Vector(0,0,0);
+								p.setVelocity(reset);
 								Diode diode = (Diode) b.getRelative(x, y, z).getState().getData();
 								BlockFace bf = diode.getFacing();
 								int delay = diode.getDelay();
-								Vector v1 = new Vector(0,0,0);
-								p.setVelocity(v1);
 								Vector v = new Vector(bf.getModX(), bf.getModY(), bf.getModZ());
-								v.setX(bf.getModX() * (delay/1.45));
-								v.setZ(bf.getModZ() * (delay/1.45));
+								v.setX(bf.getModX() * (delay));
+								v.setZ(bf.getModZ() * (delay));
 								v.setY(1);
 								System.out.println(delay);
 								p.setVelocity(v);
@@ -78,4 +87,25 @@ public class EntityListeners implements Listener {
 			}
 		}
 	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+		Player player = event.getPlayer();
+		Entity target = event.getRightClicked();
+		Entity vehicle = target.getVehicle();
+
+		if (target instanceof CraftSquid) {
+		player.setPassenger(target);
+		 ((CraftLivingEntity) target).setHealth(((CraftLivingEntity) target).getMaxHealth());
+		}
+		if (target instanceof CraftSquid && vehicle == player) {
+		((CraftSquid) target).leaveVehicle();
+		Location loc = player.getLocation();
+		loc.setY(loc.getY() + 5);
+		loc.add(player.getLocation().getDirection());
+		target.teleport(loc);
+		}
+	}
+
+
 }
